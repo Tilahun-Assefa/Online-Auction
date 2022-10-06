1681fe5fb4f2e85427561e9ddf26731b3abd1cf9
Tilahun Assefa Demissie <til.assf98@gmail.com>
2022-10-06 03:54:43 -0500

    worldcities entities added, migrations and updates done

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -3,17 +3,39 @@ using OnlineAuction.Models;
 
 namespace OnlineAuction.Data
 {
-    public class DataContext: DbContext
+    public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options): base(options) {           
-            
-         }
-         protected override void OnModelCreating (ModelBuilder modelBuilder){
-             modelBuilder.Entity<ProductCategory>().HasKey(pc => new{pc.ProductId, pc.CategoryId});
-         }
-        public DbSet<Product> Products {get; set;}
-        public DbSet<Review> Reviews {get; set;}
-        public DbSet<Category> Categories {get; set;}
-        public DbSet<ProductCategory> ProductCategories {get; set;}
+        #region Constructor
+        public DataContext() : base()
+        {
+        }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //Map Entity names to DB table names
+            modelBuilder.Entity<City>().ToTable("Cities");
+            modelBuilder.Entity<Country>().ToTable("Countries");
+
+            modelBuilder.Entity<ProductCategory>().HasKey(pc => new { pc.ProductId, pc.CategoryId });
+        }
+        #endregion Methods
+
+        #region Properties
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        #endregion Properties
     }
 }
--- Models/City.cs
+++ Models/City.cs
@@ -1,4 +1,7 @@
-﻿namespace OnlineAuction.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineAuction.Models
 {
     public class City
     {
@@ -6,6 +9,48 @@
         public City() { }
         #endregion
 
+        #region properties
+        ///<summary>
+        ///The unique id and primary key for this city
+        ///</summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
 
+        ///<summary>
+        ///City name(in UTF8 format)
+        ///</summary>
+        public string Name { get; set; }
+
+        ///<summary>
+        ///City name(in ASCII format)
+        ///</summary>
+        public string Name_ASCII { get; set; }
+
+        ///<summary>
+        ///City latitude
+        ///</summary>
+        [Column(TypeName = "decimal(7,4)")]
+        public decimal Lat { get; set; }
+
+        ///<summary>
+        ///City longitude
+        ///</summary>
+        [Column(TypeName = "decimal(7,4)")] 
+        public decimal Lon { get; set; }
+
+        ///<summary>
+        ///Country id (foreign key)
+        ///</summary>        
+        [ForeignKey("Country")]
+        public int CountryId { get; set; }
+        #endregion
+
+        #region Navigation Properties
+        ///<summary>
+        ///The Country related to this city
+        ///</summary>        
+        public virtual Country Country { get; set; }
+        #endregion
     }
 }
